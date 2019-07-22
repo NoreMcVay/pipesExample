08e9709e8fd38ce7b250742adf101c7093a75a36
Noor McVay <38887557+NoreMcVay@users.noreply.github.com>
2019-07-22 12:31:17 +0100

    Update filter.pipe.ts

--- pipesExample/app/src/app/filter.pipe.ts
+++ pipesExample/app/src/app/filter.pipe.ts
@@ -3,21 +3,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'filter',
   pure: false
-}) // 1.)impure-pipe works for every change in the component  2.)pure-pipe works only when the component is loaded.
+})
 
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, filterString: string, statusProperty: string): any {
     if (value.length === 0 || filterString === '') {
-        // filterString === '' means it will show all servers array cuh filterString is empty originally
-      // console.log(value); // this contains the array of servers
-      // console.log(status); // this displays 'status' in the console.
       return value;
     }
     const resultArray = [];
 
     for (const item of value) {
-      if (item[statusProperty] === filterString) { // if server['s status] === 'whatever typed in input'
+      if (item[statusProperty] === filterString) { 
         resultArray.push(item);
       }
     }
